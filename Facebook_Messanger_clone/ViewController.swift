693c082d7f1a4fa19d4a43cd71cfdab1c2e83e02
Steven Lin <hsuehshan@hotmail.com>
2019-09-02 14:54:01 +0800

    setup basic properties.

--- Facebook_Messanger_clone/ViewController.swift
+++ Facebook_Messanger_clone/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FriendsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
+    private let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.backgroundColor = UIColor.white
+        collectionView.alwaysBounceVertical = true
+        
+        collectionView.register(FriendCell.self, forCellWithReuseIdentifier: cellId)
     }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
 
 }
 
+class FriendCell: BaseCell {
+    
+    override func setupViews() {
+        backgroundColor = .blue
+    }
+    
+}
+
+class BaseCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = .blue
+    }
+}
